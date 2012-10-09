c787ec6490dabd6fd67bdee60625375dcaefb8ce
lingqi1818@gmail.com <lingqi1818@gmail.com@3e7b4051-d45b-7f10-da97-8eafa855eb49>
2012-10-09 07:14:39 +0000

    can run task0&task1

--- trunk/boot/head.s
+++ trunk/boot/head.s
@@ -8,6 +8,8 @@
 SCRN_SEL	= 0x18 #显存选择子
 TSS0_SEL	= 0x20
 LDT0_SEL	= 0x28
+TSS1_SEL	= 0x30
+LDT1_SEL	= 0x38
 LATCH		= 11930
 .text
 startup_32:
@@ -33,7 +35,7 @@ startup_32:
 	outb	%al,%dx
 	
 	movl	$0x00080000,%eax
-	movw	$ignore_int,%ax
+	movw	$time_interrupt,%ax
 	movw 	$0x8E00,%dx	#中断门类型，特权级为0
 	movl	$0x08,%ecx	#开机时bios设置的时钟中断向量号，这边直接使用它，你也可以通过对8259A芯片的设置改变向量号，设置方法可以参考《自己动手编写操作系统》第三章
 	lea	idt(,%ecx,8),%esi	#将idt+ecx*8的地址写入esi
@@ -123,6 +125,26 @@ system_interrupt:
 	pop %ds
 	iret
 
+time_interrupt:
+	push %ds
+	pushl %eax
+	movl $0x10, %eax
+	mov %ax, %ds
+	movb $0x20, %al
+	outb %al, $0x20	#发送EOI命令，清空ISR寄存器，让CPU能接受后面都中断。
+	movl $1, %eax
+	cmpl %eax, current
+	je 1f
+	movl %eax, current
+	ljmp $TSS1_SEL, $0
+	jmp 2f
+1:	movl $0, current
+	ljmp $TSS0_SEL, $0
+2:	popl %eax
+	pop %ds
+	iret
+
+
 setup_gdt:
 	lgdt lgdt_opcode
 	ret
@@ -159,8 +181,8 @@ gdt:	.quad 0x0000000000000000
 
 	.word 0x0068, tss0, 0xe900, 0x0	# TSS0 descr 0x20	68为104个字节，因为G=0 DPL=3 TYPE=9 可用386TSS
 	.word 0x0040, ldt0, 0xe200, 0x0	# LDT0 descr 0x28	TYPE=2 LDT
-#	.word 0x0068, tss1, 0xe900, 0x0	# TSS1 descr 0x30
-#	.word 0x0040, ldt1, 0xe200, 0x0	# LDT1 descr 0x38
+	.word 0x0068, tss1, 0xe900, 0x0	# TSS1 descr 0x30
+	.word 0x0040, ldt1, 0xe200, 0x0	# LDT1 descr 0x38
 end_gdt:
 	.fill 128,4,0
 init_stack:		#注意，汇编的堆栈是高地址向低地址伸展，所以，需要在之前开辟空闲都空间
@@ -171,16 +193,37 @@ ldt0:	.quad 0x0000000000000000
 	.quad 0x00c0fa00000003ff	# 0x0f, base = 0x00000	DPL为3
 	.quad 0x00c0f200000003ff	# 0x17	DPL为3
 
+ldt1:	.quad 0x0000000000000000
+	.quad 0x00c0fa00000003ff	# 0x0f, base = 0x00000	DPL为3
+	.quad 0x00c0f200000003ff	# 0x17	DPL为3
+
 tss0:	.long 0 			/* back link */
 	.long krn_stk0, 0x10		/* esp0, ss0 */
 	.long 0, 0, 0, 0, 0		/* esp1, ss1, esp2, ss2, cr3 */
 	.long 0, 0, 0, 0, 0		/* eip, eflags, eax, ecx, edx */
 	.long 0, 0, 0, 0, 0		/* ebx esp, ebp, esi, edi */
 	.long 0, 0, 0, 0, 0, 0 		/* es, cs, ss, ds, fs, gs */
-	.long LDT0_SEL, 0x8000000	/* ldt, trace bitmap */
+	.long LDT0_SEL, 0x8000000	/* ldt, trace bitmap 这边bitmap的地址无效，而且任务也没有IO操作，随便乱写也无所谓，具体设置方法可以参考INTEL开发手册312页，或于渊书本第三章最后一节*/
 	
 	.fill 128,4,0	#stack0堆栈大小
 krn_stk0:
+
+
+tss1:	.long 0 			/* back link */
+	.long krn_stk1, 0x10		/* esp0, ss0 */
+	.long 0, 0, 0, 0, 0		/* esp1, ss1, esp2, ss2, cr3 */
+	/*中断允许标志IF是用来决定CPU是否响应CPU外部的可屏蔽中断发出的中断请求。但不管该标志为何值，CPU都必须响应CPU外部的不可屏蔽中断所发出的中断请求，以及CPU内部产生的中断请求。具体规定如下：
+	(1)、当IF=1时，CPU可以响应CPU外部的可屏蔽中断发出的中断请求；
+	(2)、当IF=0时，CPU不响应CPU外部的可屏蔽中断发出的中断请求。*/
+	.long task1, 0x200		/* eip, eflags */
+	.long 0, 0, 0, 0		/* eax, ecx, edx, ebx */
+	.long usr_stk1, 0, 0, 0		/* esp, ebp, esi, edi */
+	.long 0x17,0x0f,0x17,0x17,0x17,0x17 /* es, cs, ss, ds, fs, gs */
+	.long LDT1_SEL, 0x8000000	/* ldt, trace bitmap 这边bitmap的地址无效，而且任务也没有IO操作，随便乱写也无所谓，具体设置方法可以参考INTEL开发手册312页，或于渊书本第三章最后一节*/
+
+	.fill 128,4,0
+krn_stk1:
+
 task0:
 	movl $0x17, %eax
 	movw %ax, %ds		#由于任务没有使用局部数据，这两句可以省略
@@ -190,4 +233,14 @@ task0:
 1:	loop 1b	#打印完A之后，休息一段时间
 	jmp task0 
 
+task1:
+	movl $0x17, %eax
+	movw %ax, %ds
+	movb $66, %al              /* print 'B' */
+	int $0x80
+	movl $0xfff, %ecx
+1:	loop 1b
+	jmp task1
 
+	.fill 128,4,0 
+usr_stk1:

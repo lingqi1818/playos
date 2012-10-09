be6b3816c32b89ecfc0a5d3c5bd9aa764791d43c
lingqi1818 <lingqi1818@gmail.com>
2012-10-09 03:14:36 +0000

    task0 can run

--- boot/head.s
+++ boot/head.s
@@ -6,6 +6,8 @@
 # 注意：此时程序已经进入保护模式，所以寻址方式为：段选择子+偏移
 .data
 SCRN_SEL	= 0x18 #显存选择子
+TSS0_SEL	= 0x20
+LDT0_SEL	= 0x28
 LATCH		= 11930
 .text
 startup_32:
@@ -29,8 +31,45 @@ startup_32:
 	outb	%al,%dx
 	movb	%ah,%al
 	outb	%al,%dx
+	
+	movl	$0x00080000,%eax
+	movw	$ignore_int,%ax
+	movw 	$0x8E00,%dx	#中断门类型，特权级为0
+	movl	$0x08,%ecx	#开机时bios设置的时钟中断向量号，这边直接使用它，你也可以通过对8259A芯片的设置改变向量号，设置方法可以参考《自己动手编写操作系统》第三章
+	lea	idt(,%ecx,8),%esi	#将idt+ecx*8的地址写入esi
+	movl	%eax,(%esi)
+	movl	%edx,4(%esi)
+	movw	$system_interrupt,%ax
+	movw 	$0xef00,%dx	#陷阱门类型15，特权级为3
+	movl	$0x80,%ecx	#系统调用向量号是0x80
+	lea	idt(,%ecx,8),%esi	#将idt+ecx*8的地址写入esi
+	movl	%eax,(%esi)
+	movl	%edx,4(%esi)
+	
+	pushfl	#将32位eflag标志寄存器入栈
+
+	#置NT标志为0
+	#嵌套任务标志NT(Nested Task)
+	#嵌套任务标志NT用来控制中断返回指令IRET的执行。具体规定如下：
+	#(1)、当NT=0，用堆栈中保存的值恢复EFLAGS、CS和EIP，执行常规的中断返回操作；
+	#(2)、当NT=1，通过任务转换实现中断返回。
+	andl	$0xffffbfff,(%esp)
+	popfl
+	movl	$TSS0_SEL,%eax		#把任务0的TSS段选择子加载到任务寄存器TR
+	ltr	%ax
+	movl	$LDT0_SEL,%eax
+	lldt	%ax
+	movl	$0,current
 	sti
-die:	jmp	die
+	#构造iret指令返回前的堆栈状况,iret需要弹出eflag，具体构造方法，参加《Orange's一个操作系统的实现》，图3.21
+	pushl	$0x17	#调用者ss 此时段选择子的ti位为1，RPL为3 数据段为0x10+7=0x17
+	pushl	$init_stack	#调用者esp
+	pushfl	#标志寄存器入栈
+	pushl	$0x0f		#调用者cs,此时段选择子的ti位为1，RPL为3,代码段为0x08+7=0x0f
+	pushl	$task0	#eip入栈
+	iret
+	
+#die:	jmp	die
 
 
 
@@ -55,6 +94,7 @@ write_char:
 
 
 scr_loc:.long 0 #屏幕当前位置
+current:.long 0
 
 ignore_int:
 	push	%ds
@@ -65,6 +105,22 @@ ignore_int:
 	call	write_char
 	popl	%eax
 	pop	%ds
+	iret
+
+system_interrupt:
+	push %ds
+	pushl %edx
+	pushl %ecx
+	pushl %ebx
+	pushl %eax
+	movl $0x10, %edx
+	mov %dx, %ds	#局部数据需要用到
+	call write_char
+	popl %eax
+	popl %ebx
+	popl %ecx
+	popl %edx
+	pop %ds
 	iret
 
 setup_gdt:
@@ -97,14 +153,41 @@ lgdt_opcode:
 idt:	.fill 256,8,0	#256个门描述符，每个8字节，门描述符和普通描述符类似，结构可以参考《自己动手编写操作系统》第三章
 
 gdt:	.quad 0x0000000000000000
-	.quad 0x00c09a00000007ff
-	.quad 0x00c09200000007ff
+	.quad 0x00c09a00000007ff	#代码段
+	.quad 0x00c09200000007ff	#数据段
 	.quad 0x00c0920b80000002	#显存段，界限为2*4k
 
+	.word 0x0068, tss0, 0xe900, 0x0	# TSS0 descr 0x20	68为104个字节，因为G=0 DPL=3 TYPE=9 可用386TSS
+	.word 0x0040, ldt0, 0xe200, 0x0	# LDT0 descr 0x28	TYPE=2 LDT
+#	.word 0x0068, tss1, 0xe900, 0x0	# TSS1 descr 0x30
+#	.word 0x0040, ldt1, 0xe200, 0x0	# LDT1 descr 0x38
 end_gdt:
 	.fill 128,4,0
+init_stack:		#注意，汇编的堆栈是高地址向低地址伸展，所以，需要在之前开辟空闲都空间
+	.long init_stack	#堆栈段偏移位置
+	.word 0x10		#堆栈段同内核数据段
 
-init_stack:		#注意，堆栈最好放在最后面，否则将会覆盖前面都指令，导致系统崩溃
-	.long init_stack
-	.word 0x10
+ldt0:	.quad 0x0000000000000000
+	.quad 0x00c0fa00000003ff	# 0x0f, base = 0x00000	DPL为3
+	.quad 0x00c0f200000003ff	# 0x17	DPL为3
+
+tss0:	.long 0 			/* back link */
+	.long krn_stk0, 0x10		/* esp0, ss0 */
+	.long 0, 0, 0, 0, 0		/* esp1, ss1, esp2, ss2, cr3 */
+	.long 0, 0, 0, 0, 0		/* eip, eflags, eax, ecx, edx */
+	.long 0, 0, 0, 0, 0		/* ebx esp, ebp, esi, edi */
+	.long 0, 0, 0, 0, 0, 0 		/* es, cs, ss, ds, fs, gs */
+	.long LDT0_SEL, 0x8000000	/* ldt, trace bitmap */
+	
+	.fill 128,4,0	#stack0堆栈大小
+krn_stk0:
+task0:
+	movl $0x17, %eax
+	movw %ax, %ds		#由于任务没有使用局部数据，这两句可以省略
+	movb $65, %al              /* print 'A' */
+	int $0x80
+	movl $0xfff, %ecx
+1:	loop 1b	#打印完A之后，休息一段时间
+	jmp task0 
+
 

33a73b2b18ca7f3881f12cfd5115692cc8abf757
lingqi1818@gmail.com <lingqi1818@gmail.com@3e7b4051-d45b-7f10-da97-8eafa855eb49>
2012-11-06 08:42:01 +0000

    modify mm/memory.c

---
+++ trunk/mm/memory.c
@@ -0,0 +1,32 @@
+/**
+ * 说明：内存0-1M处为内核和显存以及BIOS(640K-1M)
+ * 1M以上，在start_mem之前为高速缓冲区，之后为物理内存
+ * 初始化时，start_mem之前都内存分页都为USED，之后都设置为0
+ *
+ * author:ke.chenk
+ * mail:lingqi1818@gmail.com
+ *
+ */
+#define LOW_MEM 0x100000 //内存低端1M处
+#define	PAGING_MEMORY (15*1024*1024) //分页内存15M，主内存区最多15M
+#define PAGING_PAGES (PAGING_MEMORY>>12)//分页后物理内存页面数（3840）
+#define MAP_NR(addr) (((addr)-LOW_MEM)>>12)//指定内存地址映射为页号
+#define USED 100
+void write_char(char);
+static long HIGH_MEMORY = 0;
+static unsigned char mem_map [ PAGING_PAGES ] = {0,};
+void mem_init(long start_mem, long end_mem){
+	write_char('M');
+	write_char('M');
+	int i;
+	HIGH_MEMORY=end_mem;
+	for (i=0 ; i<PAGING_PAGES ; i++)
+	mem_map[i] = USED;
+	i=MAP_NR(start_mem);//主内存起始页号
+	end_mem-=start_mem;
+	end_mem>>=12;
+	while(end_mem-->0){
+		mem_map[i++]=0;
+	}
+
+}

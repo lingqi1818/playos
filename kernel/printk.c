4546962728e01ddfdb3b347f91c77cb9f2cef4a1
lingqi1818 <lingqi1818@gmail.com>
2012-11-16 02:44:38 +0000

    modify printk.c

--- kernel/printk.c
+++ kernel/printk.c
@@ -5,3 +5,13 @@
 
 static char buf[1024];
 extern int vsprintf(char * buf, const char * fmt, va_list args);
+
+int printk(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	//初始化参数列表
+	va_end(args);
+	int i = vsprintf(buf,fmt, args);
+	//TOTO 调用tty_write函数
+	return i;
+}
--- kernel/vsprintf.c
+++ kernel/vsprintf.c
@@ -4,14 +4,13 @@
 #include <stdarg.h>
 #define is_digit(c)	((c) >= '0' && (c) <= '9')
 
-static int skip_atoi(const **s){
-	int i=0;
+static int skip_atoi(const **s) {
+	int i = 0;
 
-		while (is_digit(**s))
-			i = i*10 + *((*s)++) - '0';
-		return i;
+	while (is_digit(**s))
+		i = i * 10 + *((*s)++) - '0';
+	return i;
 }
-
 
 #define ZEROPAD	1		/* pad with zero */
 #define SIGN	2		/* unsigned/signed long */
@@ -36,57 +35,64 @@ __res; })
  *precision：数字精度
  *type:flag类型
  */
-static char * number(char * str, int num, int base, int size, int precision,int type)
-{
-	char c,sign,tmp[36];
+static char * number(char * str, int num, int base, int size, int precision,
+		int type) {
+	char c, sign, tmp[36];
 	int i;
-	const char *digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	if (type&SMALL) digits="0123456789abcdefghijklmnopqrstuvwxyz";
-	if (type&LEFT) type &= ~ZEROPAD;//如果类型为左对齐，则屏蔽填0标志位
-	if (base<2 || base>36)//不支持的进制
-	return 0;
-	c = (type & ZEROPAD) ? '0' : ' ' ;
-	if (type&SIGN && num<0) {//判断标志符为：+，-或' '.
-		sign='-';
-		num = -num;//负数
+	const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (type & SMALL)
+		digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+	if (type & LEFT)
+		type &= ~ZEROPAD; //如果类型为左对齐，则屏蔽填0标志位
+	if (base < 2 || base > 36) //不支持的进制
+		return 0;
+	c = (type & ZEROPAD) ? '0' : ' ';
+	if (type & SIGN && num < 0) { //判断标志符为：+，-或' '.
+		sign = '-';
+		num = -num; //负数
 	} else
-		sign=(type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
-	if (sign) size--; //符号不为0
-	if (type&SPECIAL)//特殊类型符号
-			if (base==16) size -= 2;//0x size-2
-			else if (base==8) size--;//0 size-1
-	i=0;
-	if (num==0)
-		tmp[i++]='0';
-	else while (num!=0)
-		tmp[i++]=digits[do_div(num,base)];
-	if (i>precision) precision=i;
-		size -= precision;
-	if (!(type&(ZEROPAD+LEFT)))//不是左对齐，且补空格
-		while(size-->0)
+		sign = (type & PLUS) ? '+' : ((type & SPACE) ? ' ' : 0);
+	if (sign)
+		size--; //符号不为0
+	if (type & SPECIAL) //特殊类型符号
+		if (base == 16)
+			size -= 2; //0x size-2
+		else if (base == 8)
+			size--; //0 size-1
+	i = 0;
+	if (num == 0)
+		tmp[i++] = '0';
+	else
+		while (num != 0)
+			tmp[i++] = digits[do_div(num,base)];
+	if (i > precision)
+		precision = i;
+	size -= precision;
+	if (!(type & (ZEROPAD + LEFT))) //不是左对齐，且补空格
+		while (size-- > 0)
 			*str++ = ' ';
-	if (sign)//符号
+	if (sign) //符号
 		*str++ = sign;
-	if (type&SPECIAL)
-		if (base==8)
+	if (type & SPECIAL)
+		if (base == 8)
 			*str++ = '0';
-		else if (base==16) {
+		else if (base == 16) {
 			*str++ = '0';
 			*str++ = digits[33];
 		}
-	if (!(type&LEFT))
-		while(size-->0)
+	if (!(type & LEFT))
+		while (size-- > 0)
 			*str++ = c;
-	while(i<precision--)
+	while (i < precision--)
 		*str++ = '0';
-	while(i-->0)
+	while (i-- > 0)
 		*str++ = tmp[i];
-	while(size-->0)
+	while (size-- > 0)
 		*str++ = ' ';
 	return str;
 }
 
-int vsprintf(char *buf, const char *fmt, va_list args){
+int vsprintf(char *buf, const char *fmt, va_list args) {
 	int *ip;
 	int len;
 	int i;
@@ -96,33 +102,42 @@ int vsprintf(char *buf, const char *fmt, va_list args){
 	int field_width;
 	int precision;
 	int qualifier;
-	for (str=buf ; *fmt ; ++fmt) {
+	for (str = buf; *fmt; ++fmt) {
 		if (*fmt != '%') {
 			*str++ = *fmt;
 			continue;
 		}
-	repeat:
-	flags=0;
-	++fmt;//skip '%'
-	switch (*fmt) {
-		case '-': flags |= LEFT; goto repeat;
-		case '+': flags |= PLUS; goto repeat;
-		case ' ': flags |= SPACE; goto repeat;
-		case '#': flags |= SPECIAL; goto repeat;
-		case '0': flags |= ZEROPAD; goto repeat;
-		}
-	field_width = -1;
-	if (is_digit(*fmt))//如果为数字，则直接取数字，如果为*则取下个参数值
-		field_width = skip_atoi(&fmt);
-	else if (*fmt == '*') {
-		field_width = va_arg(args, int);
-		if (field_width < 0) {
-			field_width = -field_width;
+		repeat: flags = 0;
+		++fmt; //skip '%'
+		switch (*fmt) {
+		case '-':
 			flags |= LEFT;
+			goto repeat;
+		case '+':
+			flags |= PLUS;
+			goto repeat;
+		case ' ':
+			flags |= SPACE;
+			goto repeat;
+		case '#':
+			flags |= SPECIAL;
+			goto repeat;
+		case '0':
+			flags |= ZEROPAD;
+			goto repeat;
 		}
-	}
+		field_width = -1;
+		if (is_digit(*fmt)) //如果为数字，则直接取数字，如果为*则取下个参数值
+			field_width = skip_atoi(&fmt);
+		else if (*fmt == '*') {
+			field_width = va_arg(args, int);
+			if (field_width < 0) {
+				field_width = -field_width;
+				flags |= LEFT;
+			}
+		}
 
-	/* get the precision */
+		/* get the precision */
 		precision = -1;
 		if (*fmt == '.') {
 			++fmt;
@@ -143,75 +158,73 @@ int vsprintf(char *buf, const char *fmt, va_list args){
 			++fmt;
 		}
 
-
 		switch (*fmt) {
-			case 'c':
-					if (!(flags & LEFT))//不是左对齐，则在前面补空格，否则在后面补空格
-						while (--field_width > 0)
-							*str++ = ' ';
-					*str++ = (unsigned char) va_arg(args, int);
-					while (--field_width > 0)
-						*str++ = ' ';
-					break;
+		case 'c':
+			if (!(flags & LEFT)) //不是左对齐，则在前面补空格，否则在后面补空格
+				while (--field_width > 0)
+					*str++ = ' ';
+			*str++ = (unsigned char) va_arg(args, int);
+			while (--field_width > 0)
+				*str++ = ' ';
+			break;
 
-			 case 's':
-					s = va_arg(args, char *);
-					len = strlen(s);
-					if (precision < 0)
-						precision = len;
-					else if (len > precision)
-						len = precision;
+		case 's':
+			s = va_arg(args, char *);
+			len = strlen(s);
+			if (precision < 0)
+				precision = len;
+			else if (len > precision)
+				len = precision;
 
-					if (!(flags & LEFT))
-						while (len < field_width--)
-							*str++ = ' ';
-					for (i = 0; i < len; ++i)
-						*str++ = *s++;
-					while (len < field_width--)
-						*str++ = ' ';
-					break;
+			if (!(flags & LEFT))
+				while (len < field_width--)
+					*str++ = ' ';
+			for (i = 0; i < len; ++i)
+				*str++ = *s++;
+			while (len < field_width--)
+				*str++ = ' ';
+			break;
 
-			 case 'o':
-					str = number(str, va_arg(args, unsigned long), 8,
-						field_width, precision, flags);
-					break;
-			 case 'p'://指针类型
-					if (field_width == -1) {
-						field_width = 8;
-						flags |= ZEROPAD;
-					}
-					str = number(str,
-						(unsigned long) va_arg(args, void *), 16,
-						field_width, precision, flags);
-					break;
-			 case 'x'://16进制
-					flags |= SMALL;
-				case 'X':
-					str = number(str, va_arg(args, unsigned long), 16,
-						field_width, precision, flags);
-					break;
-			case 'd'://整数
-			case 'i'://有符号
-				flags |= SIGN;
-			case 'u'://无符号
-				str = number(str, va_arg(args, unsigned long), 10,
+		case 'o':
+			str = number(str, va_arg(args, unsigned long), 8, field_width,
+					precision, flags);
+			break;
+		case 'p': //指针类型
+			if (field_width == -1) {
+				field_width = 8;
+				flags |= ZEROPAD;
+			}
+			str = number(str, (unsigned long) va_arg(args, void *), 16,
 					field_width, precision, flags);
-				break;
-			case 'n'://停止打印，并将已经打印的字符数写入到int指针指向的地址
-				ip = va_arg(args, int *);
-				*ip = (str - buf);
-				break;
-			default:
-				if (*fmt != '%')
-					*str++ = '%';
-				if (*fmt)
-					*str++ = *fmt;
-				else
-					--fmt;
-				break;
+			break;
+		case 'x': //16进制
+			flags |= SMALL;
+		case 'X':
+			str = number(str, va_arg(args, unsigned long), 16, field_width,
+					precision, flags);
+			break;
+		case 'd': //整数
+		case 'i': //有符号
+			flags |= SIGN;
+		case 'u': //无符号
+			str = number(str, va_arg(args, unsigned long), 10, field_width,
+					precision, flags);
+			break;
+		case 'n': //停止打印，并将已经打印的字符数写入到int指针指向的地址
+			ip = va_arg(args, int *);
+			*ip = (str - buf);
+			break;
+		default:
+			if (*fmt != '%')
+				*str++ = '%';
+			if (*fmt)
+				*str++ = *fmt;
+			else
+				--fmt;
+			break;
 		}
 
 	}
 	*str = '\0';
-	return str-buf;
+	return str - buf;
 }

a8d9885309b32950e19fc47cf90db9eb393d7b83
lingqi1818 <lingqi1818@gmail.com>
2013-01-11 07:47:30 +0000

    only for ci

--- include/sys/types.h
+++ include/sys/types.h
@@ -5,4 +5,6 @@
 #define NULL ((void *) 0)
 #endif
 
+typedef long off_t;//文件的长度
+
 #endif
---
+++ kernel/printf.c
@@ -0,0 +1,15 @@
+/**
+ * 向终端输出格式化的字符串
+ */
+static char printbuf[1024];
+
+int printf(const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	write(1,printbuf,i=vsprintf(printbuf, fmt, args));
+	va_end(args);
+	return i;
+}

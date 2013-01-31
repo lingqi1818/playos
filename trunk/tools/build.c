2262bf8e1aadee728fdac66cd12fca0f7c5961d5
lingqi1818@gmail.com <lingqi1818@gmail.com@3e7b4051-d45b-7f10-da97-8eafa855eb49>
2013-01-31 09:28:29 +0000

    fix the lss SS is null selector bug

--- trunk/tools/build.c
+++ trunk/tools/build.c
@@ -1,68 +1,83 @@
 /*
- *本程序主要将以下三个文件生成磁盘映像文件
- * bootsec：启动扇区（512字节）
- * setup:设置系统参数，占用4个扇区
- * system:实际内核代码
- * 
- * autor:ke.chenk
- * mail:lingqi1818@msn.com
+ *  linux/tools/build.c
+ *
+ *  (C) 1991  Linus Torvalds
  */
-#include <stdio.h>
+
+/*
+ * This file builds a disk-image from three different files:
+ *
+ * - bootsect: max 510 bytes of 8086 machine code, loads the rest
+ * - setup: max 4 sectors of 8086 machine code, sets up system parm
+ * - system: 80386 code for actual system
+ *
+ * It does some checking that all files are of the correct type, and
+ * just writes the result to stdout, removing headers and padding to
+ * the right amount. It also writes some system data to stderr.
+ */
+
+/*
+ * Changes by tytso to allow root device specification
+ */
+
+#include <stdio.h>	/* fprintf */
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h>	/* contains exit */
+#include <sys/types.h>	/* unistd.h needs this */
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <include/linux/fs.h>
-#include <unistd.h>
+#include <unistd.h>	/* contains read/write */
 #include <fcntl.h>
-
-
-#define DEFAULT_MAJOR_ROOT 3
-#define DEFAULT_MINOR_ROOT 6	// dev/hd6 第二个硬盘第一个分区
-
-
+#define MAJOR(a) (((unsigned)(a))>>8)
+#define MINOR(a) ((a)&0xff)
 #define MINIX_HEADER 32
 #define GCC_HEADER 1024
-#define SYS_SIZE 0x2000
+
+#define SYS_SIZE 0x3000
+
+#define DEFAULT_MAJOR_ROOT 3
+#define DEFAULT_MINOR_ROOT 6
+
+/* max nr of sectors of setup: don't change unless you also change
+ * bootsect etc */
 #define SETUP_SECTS 4
 
-void die(char *str)
+#define STRINGIFY(x) #x
+
+void die(char * str)
 {
-    fprintf(stderr,"%s\n",str);
-    exit(1);
+	fprintf(stderr,"%s\n",str);
+	exit(1);
 }
 
 void usage(void)
 {
-    die("Usage: build bootsec setup system [rootdev] [> image]");
+	die("Usage: build bootsect setup system [rootdev] [> image]");
 }
-int main(int argc,char **argv)
+
+int main(int argc, char ** argv)
 {
-    int i,c,id;
-    char buf[1024];
-    char major_root,minor_root;
-    struct stat sb;
-    if (argc!=4 && argc!=5)
-        usage();
-    
-    if (argc==5)
-    {
-        if(strcmp(argv[4],"FLOPPY"))   
-        {
-            if(stat(argv[4],&sb))
-            {
-                perror(argv[4]);
-                die("couldn't stat root device .");
-            }
-            major_root = MAJOR(sb.st_rdev);
-            minor_root = MINOR(sb.st_rdev);
-        }else{
-	    major_root = 0;
-            minor_root = 0;
-	}
-    }else{
-	    major_root = DEFAULT_MAJOR_ROOT;
-	    minor_root = DEFAULT_MINOR_ROOT;
+	int i,c,id;
+	char buf[1024];
+	char major_root, minor_root;
+	struct stat sb;
+
+	if ((argc != 4) && (argc != 5))
+		usage();
+	if (argc == 5) {
+		if (strcmp(argv[4], "FLOPPY")) {
+			if (stat(argv[4], &sb)) {
+				perror(argv[4]);
+				die("Couldn't stat root device.");
+			}
+			major_root = MAJOR(sb.st_rdev);
+			minor_root = MINOR(sb.st_rdev);
+		} else {
+			major_root = 0;
+			minor_root = 0;
+		}
+	} else {
+		major_root = DEFAULT_MAJOR_ROOT;
+		minor_root = DEFAULT_MINOR_ROOT;
 	}
 	fprintf(stderr, "Root device is (%d, %d)\n", major_root, minor_root);
 	if ((major_root != 2) && (major_root != 3) &&
@@ -72,11 +87,6 @@ int main(int argc,char **argv)
 		die("Bad root device --- major #");
 	}
 	for (i=0;i<sizeof buf; i++) buf[i]=0;
-	//read bootsect
-	if ((id=open(argv[1],O_RDONLY,0))<0)
-		die("Unable to open 'boot'");
-	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
-		die("Unable to read header of 'boot'");
 	if ((id=open(argv[1],O_RDONLY,0))<0)
 		die("Unable to open 'boot'");
 	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
@@ -98,7 +108,7 @@ int main(int argc,char **argv)
 	if (i != 512)
 		die("Boot block must be exactly 512 bytes");
 	if ((*(unsigned short *)(buf+510)) != 0xAA55)
-	die("Boot block hasn't got boot flag (0xAA55)");
+		die("Boot block hasn't got boot flag (0xAA55)");
 	buf[508] = (char) minor_root;
 	buf[509] = (char) major_root;
 	i=write(1,buf,512);
@@ -106,7 +116,6 @@ int main(int argc,char **argv)
 		die("Write call failed");
 	close (id);
 
-	// read setup
 	if ((id=open(argv[2],O_RDONLY,0))<0)
 		die("Unable to open 'setup'");
 	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
@@ -123,7 +132,7 @@ int main(int argc,char **argv)
 		die("Non-Minix header of 'setup'");
 	if (((long *) buf)[7] != 0)
 		die("Illegal symbol table in 'setup'");
-        for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
+	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
 		if (write(1,buf,c)!=c)
 			die("Write call failed");
 	close (id);
@@ -131,11 +140,9 @@ int main(int argc,char **argv)
 		die("Setup exceeds " STRINGIFY(SETUP_SECTS)
 			" sectors - rewrite build/boot/setup");
 	fprintf(stderr,"Setup is %d bytes.\n",i);
-        //清空缓冲区
-        for (c=0 ; c<sizeof(buf) ; c++)
+	for (c=0 ; c<sizeof(buf) ; c++)
 		buf[c] = '\0';
-	//若setup未满4个扇区，则余下部分补0
-        while (i<SETUP_SECTS*512) {
+	while (i<SETUP_SECTS*512) {
 		c = SETUP_SECTS*512-i;
 		if (c > sizeof(buf))
 			c = sizeof(buf);
@@ -143,20 +150,19 @@ int main(int argc,char **argv)
 			die("Write call failed");
 		i += c;
 	}
-        
-        //read system
-        if ((id=open(argv[3],O_RDONLY,0))<0)
+
+	if ((id=open(argv[3],O_RDONLY,0))<0)
 		die("Unable to open 'system'");
-	if (read(id,buf,GCC_HEADER) != GCC_HEADER)
-		die("Unable to read header of 'system'");
-	if (((long *) buf)[5] != 0)
-		die("Non-GCC header of 'system'");
+//	if (read(id,buf,GCC_HEADER) != GCC_HEADER)
+//		die("Unable to read header of 'system'");
+//	if (((long *) buf)[5] != 0)
+//		die("Non-GCC header of 'system'");
 	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
 		if (write(1,buf,c)!=c)
 			die("Write call failed");
 	close(id);
 	fprintf(stderr,"System is %d bytes.\n",i);
-	if (i > SYS_SIZE*16)    //系统长度超过128KB则退出
+	if (i > SYS_SIZE*16)
 		die("System is too big");
 	return(0);
 }

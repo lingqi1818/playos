03ae51b3bd9a90a87f3ee2a0af76919146b2f522
lingqi1818@gmail.com <lingqi1818@gmail.com@3e7b4051-d45b-7f10-da97-8eafa855eb49>
2013-02-22 03:42:52 +0000

    only for ci

--- trunk/fs/super.c
+++ trunk/fs/super.c
@@ -1,1 +1,17 @@
+#include <linux/fs.h>
 int ROOT_DEV = 0;
+void mount_root(void)
+{
+	int i,free;
+		struct super_block * p;
+		struct m_inode * mi;
+
+		if (32 != sizeof (struct d_inode))
+			panic("bad i-node size");
+		for(i=0;i<NR_FILE;i++)
+			file_table[i].f_count=0;
+		if (MAJOR(ROOT_DEV) == 2) {
+			printk("Insert root floppy and press ENTER");
+			wait_for_keypress();
+		}
+}

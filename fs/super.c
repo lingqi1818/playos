e4ce8fce5844f18ef47cb441eb4ece4c0b0dc1eb
lingqi1818 <lingqi1818@gmail.com>
2013-03-06 03:05:21 +0000

    only for ci

--- fs/super.c
+++ fs/super.c
@@ -3,6 +3,14 @@
 struct super_block super_block[NR_SUPER];
 int ROOT_DEV = 0;
 
+int sync_dev(int dev);
+void wait_for_keypress(void);
+
+#define set_bit(bitnr,addr) ({ \
+register int __res __asm__("ax"); \
+__asm__("bt %2,%3;setb %%al":"=a" (__res):"a" (0),"r" (bitnr),"m" (*(addr))); \
+__res; })
+
 static void lock_super(struct super_block * sb)
 {
 	cli();
@@ -28,22 +36,6 @@ static void wait_on_super(struct super_block * sb)
 	sti();
 }
 
-void mount_root(void)
-{
-	int i,free;
-		struct super_block * p;
-		struct m_inode * mi;
-
-		if (32 != sizeof (struct d_inode))
-			panic("bad i-node size");
-		for(i=0;i<NR_FILE;i++)
-			file_table[i].f_count=0;
-		if (MAJOR(ROOT_DEV) == 2) {
-			printk("Insert root floppy and press ENTER");
-			//wait_for_keypress();
-		}
-}
-
 
 struct super_block * get_super(int dev)
 {
@@ -61,4 +53,48 @@ struct super_block * get_super(int dev)
 		} else
 			s++;
 	return NULL;
+}
+
+/**
+ * 挂载根文件系统
+ */
+void mount_root(void)
+{
+	int i,free;
+	struct super_block * p;
+	struct m_inode * mi;
+
+	if (32 != sizeof (struct d_inode))
+		panic("bad i-node size");
+	for(i=0;i<NR_FILE;i++)
+		file_table[i].f_count=0;
+	if (MAJOR(ROOT_DEV) == 2) {
+		printk("Insert root floppy and press ENTER");
+		wait_for_keypress();
+	}
+	for(p = &super_block[0] ; p < &super_block[NR_SUPER] ; p++) {
+		p->s_dev = 0;
+		p->s_lock = 0;
+		p->s_wait = NULL;
+	}
+	if (!(p=read_super(ROOT_DEV)))
+		panic("Unable to mount root");
+	if (!(mi=iget(ROOT_DEV,ROOT_INO)))
+		panic("Unable to read root i-node");
+	mi->i_count += 3 ;	/* NOTE! it is logically used 4 times, not 1 */
+	p->s_isup = p->s_imount = mi;
+	current->pwd = mi;
+	current->root = mi;
+	free=0;
+	i=p->s_nzones;
+	while (-- i >= 0)
+		if (!set_bit(i&8191,p->s_zmap[i>>13]->b_data))
+			free++;
+	printk("%d/%d free blocks\n\r",free,p->s_nzones);
+	free=0;
+	i=p->s_ninodes+1;
+	while (-- i >= 0)
+		if (!set_bit(i&8191,p->s_imap[i>>13]->b_data))
+			free++;
+	printk("%d/%d free inodes\n\r",free,p->s_ninodes);
 }
